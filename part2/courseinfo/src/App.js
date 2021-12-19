90105b46bb6cf6fa6a9bc43b166a3515a0944784
Ashutosh <iashutoshmishra8@gmail.com>
2021-12-19 13:09:54 +0530

    Ex 2.1 to 2.5 finished

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,99 @@
+import React from "react";
+
+const Header = ({ courses }) => {
+    return <h1>{courses}</h1>
+  }
+
+const Total = ({ courses }) => {
+  const sum = courses.reduce((previous, current) => previous + current.exercises, 0)
+  return <p><strong>total of {sum} exercises </strong></p>
+}
+
+const Part = ({part}) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+      {/* {console.log(part.name)} */}
+    </p>
+  )
+}
+
+const Content = ({ courses }) => {
+  return courses.map((course) => {
+      return(
+        <div>
+          <Header 
+            key={course.id}
+            courses={course.name} 
+          />
+          {course.parts.map((item, key) => {
+            return (
+                <Part
+                  key={key}
+                  part={item}
+                />
+            )
+          })}
+          <Total courses={course.parts} />
+        </div>
+      )
+    })}
+
+const Course = ({ courses }) => {
+  return (
+    <div>
+      <Header courses='Web development curriculum' />
+      <Content courses={courses} />
+    </div>
+  )
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  return <Course courses={courses} />
+}
+
+export default App
