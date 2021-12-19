1c456a270b7f4521b50679bb944ff83fae610323
Ashutosh <iashutoshmishra8@gmail.com>
2021-12-19 19:02:55 +0530

    minor changes

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,52 +1,5 @@
 import React from "react";
-
-const Header = ({ courses }) => {
-    return <h1>{courses}</h1>
-  }
-
-const Total = ({ courses }) => {
-  const sum = courses.reduce((previous, current) => previous + current.exercises, 0)
-  return <p><strong>total of {sum} exercises </strong></p>
-}
-
-const Part = ({part}) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-      {/* {console.log(part.name)} */}
-    </p>
-  )
-}
-
-const Content = ({ courses }) => {
-  return courses.map((course) => {
-      return(
-        <div>
-          <Header 
-            key={course.id}
-            courses={course.name} 
-          />
-          {course.parts.map((item, key) => {
-            return (
-                <Part
-                  key={key}
-                  part={item}
-                />
-            )
-          })}
-          <Total courses={course.parts} />
-        </div>
-      )
-    })}
-
-const Course = ({ courses }) => {
-  return (
-    <div>
-      <Header courses='Web development curriculum' />
-      <Content courses={courses} />
-    </div>
-  )
-}
+import Course from './Course'
 
 const App = () => {
   const courses = [
