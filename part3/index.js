64110aa1df2a44cd40b476a508fdb3f11b74e1c6
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-03-31 15:55:38 +0530

    exercise 3.2 finished

--- part3/index.js
+++ part3/index.js
@@ -24,11 +24,19 @@ let persons = [
   },
 ];
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-const PORT = 3001
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${
+      persons.length
+    } people</p> <p>${new Date()}</p>`
+  );
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Port running on server ${PORT}`)
-})
+  console.log(`Port running on server ${PORT}`);
+});
