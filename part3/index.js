28ccee22e6017640031b7c300a869069575b0836
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-04-23 01:01:52 +0530

    get and delete update

--- part3/index.js
+++ part3/index.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
-const Person = require('./models/person')
+const Person = require("./models/person");
 
-app.use(cors())
-app.use(express.static('build'))
+app.use(cors());
+app.use(express.static("build"));
 app.use(morgan("tiny"));
 app.use(
   morgan(":method :url :status :res[content-length] :response-time ms :body")
@@ -37,9 +37,9 @@ let persons = [
 ];
 
 app.get("/api/persons", (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (request, response) => {
@@ -50,17 +50,24 @@ app.get("/info", (request, response) => {
   );
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end;
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 // random id number needed to assign to post requests body
@@ -81,8 +88,8 @@ app.post("/api/persons", (request, response) => {
 
   const person = new Person({
     name: body.name,
-    number: body.number
-  })
+    number: body.number,
+  });
 
   // if (persons.find((person) => person.name === body.name)) {
   //   return response.status(400).json({
@@ -90,9 +97,9 @@ app.post("/api/persons", (request, response) => {
   //   });
   // }
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 morgan.token("body", (req, res) => JSON.stringify(req.body));
