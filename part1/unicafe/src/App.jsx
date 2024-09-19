be7675ff77662897b54f111379822f0935dadbee
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2024-09-19 14:34:35 +0530

    Add exercise 1.10 part 2(button component)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+const Button = ({ text, onClick }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
 const StatisticLine = ({ text, value }) => {
   return (
     <p>
@@ -57,9 +61,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={handleGood}>good</button>
-      <button onClick={handleNeutral}>neutral</button>
-      <button onClick={handleBad}>bad</button>
+      <Button onClick={handleGood} text="good" />
+      <Button onClick={handleNeutral} text="neutral" />
+      <Button onClick={handleBad} text="bad" />
       <Statistics good={good} neutral={neutral} bad={bad} all={all} />
     </div>
   );
